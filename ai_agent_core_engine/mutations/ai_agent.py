e4394929cf301eccf360608d7fd79ade0550012b
Idea Bosque <ideabosque@gmail.com>
2025-03-17 22:23:05 -0700

    Full cycle implemnt for no stream.

---
+++ ai_agent_core_engine/mutations/ai_agent.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+__author__ = "bibow"
+
+import traceback
+from typing import Any, Dict
+
+from graphene import Boolean, Mutation, String
+
+from silvaengine_utility import JSON
+
+from ..handlers.at_agent_listener import execute_ask_model
+
+
+class ExecuteAskModel(Mutation):
+    ok = Boolean()
+
+    class Arguments:
+        async_task_uuid = String(required=True)
+        arguments = JSON(required=True)
+
+    @staticmethod
+    def mutate(root: Any, info: Any, **kwargs: Dict[str, Any]) -> "ExecuteAskModel":
+        try:
+            ok = execute_ask_model(info, **kwargs)
+        except Exception as e:
+            log = traceback.format_exc()
+            info.context.get("logger").error(log)
+            raise e
+
+        return ExecuteAskModel(ok=ok)
