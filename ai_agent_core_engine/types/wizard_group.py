27508a8f6e0a3024f9f472b90a7a14ecbf4ac4d5
Barry Liu <barry.y.liu@hotmail.com>
2025-12-30 18:04:16 +0800

    [U] Add debug info

--- ai_agent_core_engine/types/wizard_group.py
+++ ai_agent_core_engine/types/wizard_group.py
@@ -59,18 +59,17 @@ class WizardGroupType(ObjectType):
             for wizard_uuid in wizard_uuids
         ]
 
-        result = Promise.all(promises).then(
-            lambda wizard_dicts: (
-                [normalize_to_json(wizard_dict) for wizard_dict in wizard_dicts]
-                if wizard_dicts
-                else []
+        return (
+            Promise.all(promises)
+            .then(
+                lambda wizard_dicts: (
+                    [normalize_to_json(wizard_dict) for wizard_dict in wizard_dicts]
+                    if wizard_dicts
+                    else []
+                )
             )
+            .get()
         )
-
-        print(f"Promises Result {'>' * 60}", type(result))
-        print(f"Promises Result {'>' * 60}", result)
-
-        return result
 
 
 class WizardGroupListType(ListObjectType):
