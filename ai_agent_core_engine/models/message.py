cc7680f0a1044e3f853e85a509c9205e43d17b48
Idea Bosque <ideabosque@gmail.com>
2026-02-26 23:59:54 -0800

    Fix the token count issue.

--- ai_agent_core_engine/models/message.py
+++ ai_agent_core_engine/models/message.py
@@ -12,6 +12,8 @@ import pendulum
 from graphene import ResolveInfo
 from pynamodb.attributes import UnicodeAttribute, UTCDateTimeAttribute
 from pynamodb.indexes import AllProjection, LocalSecondaryIndex
+from tenacity import retry, stop_after_attempt, wait_exponential
+
 from silvaengine_dynamodb_base import (
     BaseModel,
     delete_decorator,
@@ -20,7 +22,6 @@ from silvaengine_dynamodb_base import (
     resolve_list_decorator,
 )
 from silvaengine_utility import method_cache
-from tenacity import retry, stop_after_attempt, wait_exponential
 
 from ..handlers.config import Config
 from ..types.message import MessageListType, MessageType
